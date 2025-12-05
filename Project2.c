cf2ed6b2118c348fa50522b098213c34f7fc4732
Black-Aichi <tyler.stout@bsu.edu>
2025-12-05 09:21:14 -0500

    Update Project 2
    Now converts input to corresponding integer value

--- Project2.c
+++ Project2.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 
 int main(){
-    char input ;
+    char input = 0  ;
     while(input != 'x'){
         printf("Enter a Hexadecimal digit (or 'x' to exit): ");
         scanf(" %c", &input);
-        int int_val = input - '0';
-        printf("the value is %d\n", int_val);
-    }
-    
 
+        if (input == 'x'){
+            break;
+        }
+
+        int int_val;
+
+        if (input >= '0' && input <= '9'){
+            int_val = input -'0';
+        }
+        else if (input >='A' && input <= 'F'){
+            int_val = input -'A'+ 10;
+        }
+        else if (input >= 'a' && input <= 'f'){
+            int_val = input -'a' + 10;
+        }
+        else{
+            printf("Invalid hexadecimal value.\n");
+            continue;
+        }
+
+        printf("the value is %d\n", int_val);
+
+
+    }
     return 0;
 }
